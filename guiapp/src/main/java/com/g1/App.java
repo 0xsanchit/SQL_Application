f7ed81d352749b28a1ac249abb078ec22b59b0a8
VedantS01 <vedantsaboo2001@gmail.com>
2021-10-28 21:48:41 +0530

    v1.1

---
+++ guiapp/src/main/java/com/g1/App.java
@@ -0,0 +1,27 @@
+package com.g1;
+
+import java.awt.EventQueue;
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UserLogin frame = new UserLogin();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
--- guiapp/src/main/java/com/g1/UserLogin.java
+++ guiapp/src/main/java/com/g1/UserLogin.java
@@ -1,31 +1,22 @@
+package com.g1;
+
 // package BasicForm.src;
 
 // import BasicForm.*;
 
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JrollnoField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import javax.swing.*;
-import java.awt.*;
-import java.util.*;
 
-public class Form extends JFrame {
+public class UserLogin extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private JTextField nameField;
@@ -41,9 +32,8 @@ public class Form extends JFrame {
     //     EventQueue.invokeLater(new Runnable() {
     //         public void run() {
     //             try {
-    //                 // HomeFrame frame = new HomeFrame();
-    //                 // frame.setVisible(true);
-    //                 new Form().setVisible(true);
+    //                 UserLogin frame = new UserLogin();
+    //                 frame.setVisible(true);
     //             } catch (Exception e) {
     //                 e.printStackTrace();
     //             }
@@ -54,7 +44,7 @@ public class Form extends JFrame {
     /**
      * Create the frame.
      */
-    public Form() {
+    public UserLogin() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(450, 190, 1014, 597);
         setResizable(true);
@@ -81,27 +71,50 @@ public class Form extends JFrame {
         rollnoField.setBounds(481, 286, 281, 68);
         contentPane.add(rollnoField);
 
-        JLabel lblname = new JLabel("Name");
-        lblname.setBackground(Color.BLACK);
-        lblname.setForeground(Color.BLACK);
-        lblname.setFont(new Font("Tahoma", Font.PLAIN, 31));
-        lblname.setBounds(250, 166, 193, 52);
-        contentPane.add(lblname);
+        JLabel lblUsername = new JLabel("Name");
+        lblUsername.setBackground(Color.BLACK);
+        lblUsername.setForeground(Color.BLACK);
+        lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 31));
+        lblUsername.setBounds(250, 166, 193, 52);
+        contentPane.add(lblUsername);
 
-        JLabel lblrollno = new JLabel("Roll Number");
-        lblrollno.setForeground(Color.BLACK);
-        lblrollno.setBackground(Color.CYAN);
-        lblrollno.setFont(new Font("Tahoma", Font.PLAIN, 31));
-        lblrollno.setBounds(250, 286, 193, 52);
-        contentPane.add(lblrollno);
+        JLabel lblPassword = new JLabel("Roll Number");
+        lblPassword.setForeground(Color.BLACK);
+        lblPassword.setBackground(Color.CYAN);
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 31));
+        lblPassword.setBounds(250, 286, 193, 52);
+        contentPane.add(lblPassword);
 
-        btnNewButton = new JButton("Enter details");
+        btnNewButton = new JButton("Login");
         btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
         btnNewButton.setBounds(545, 392, 162, 73);
         btnNewButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-                // do something here :)
+                // String userName = nameField.getText();
+                // String password = rollnoField.getText();
+                // try {
+                //     Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/swing_demo",
+                //         "root", "root");
+
+                //     PreparedStatement st = (PreparedStatement) connection
+                //         .prepareStatement("Select name, password from student where name=? and password=?");
+
+                //     st.setString(1, userName);
+                //     st.setString(2, password);
+                //     ResultSet rs = st.executeQuery();
+                //     if (rs.next()) {
+                //         dispose();
+                //         UserHome ah = new UserHome(userName);
+                //         ah.setTitle("Welcome");
+                //         ah.setVisible(true);
+                //         JOptionPane.showMessageDialog(btnNewButton, "You have successfully logged in");
+                //     } else {
+                //         JOptionPane.showMessageDialog(btnNewButton, "Wrong Username & Password");
+                //     }
+                // } catch (SQLException sqlException) {
+                //     sqlException.printStackTrace();
+                // }
             }
         });
 
