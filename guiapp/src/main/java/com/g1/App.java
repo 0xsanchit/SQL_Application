b4cd4690e602660e8aa3c001f8dbae6c3575e8ea
VedantS01 <vedantsaboo2001@gmail.com>
2021-10-29 11:40:38 +0530

    v1.1.1.0

--- guiapp/src/main/java/com/g1/App.java
+++ guiapp/src/main/java/com/g1/App.java
@@ -1,6 +1,8 @@
 package com.g1;
 
 import java.awt.EventQueue;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  * Hello world!
  */
@@ -11,13 +13,45 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws SQLException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        SQLConnection sqlConnection = new SQLConnection();
+        boolean status = false;
+        try {
+            status = sqlConnection.connect("<YOURUSERNAME>", "<YOURPASSWORD>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (status == true) {
+            ResultSet resultSet = sqlConnection.executeQuery("select * from student");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+            sqlConnection.closeConnection();
+        } else {
+            System.out.println("Connection Failed");
+        }
+        if(status)
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
                     // UserLogin frame = new UserLogin();
-                    HomeFrame frame = new HomeFrame();
+                    HomeFrame frame;
+                    frame = new HomeFrame(sqlConnection);
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        else
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    // UserLogin frame = new UserLogin();
+                    HomeFrame frame;
+                    frame = new HomeFrame();
                     frame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
--- guiapp/src/main/java/com/g1/HomeFrame.java
+++ guiapp/src/main/java/com/g1/HomeFrame.java
@@ -13,6 +13,9 @@ import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
 import java.awt.FlowLayout;
 
 public class HomeFrame extends JFrame {
@@ -58,6 +61,87 @@ public class HomeFrame extends JFrame {
         contentPane.add(selectDepartment);
 
         dropdownDept = new JComboBox<>(tryoutlist);
+        // dropdownDept.setBounds(70, 200, 200, 100);
+        dropdownDept.setBorder(new EmptyBorder(5, 5, 5, 5));
+        dropdownDept.setFont(new Font("Times New Roman", Font.PLAIN, 40));
+        dropdownDept.setForeground(Color.BLACK);
+        dropdownDept.setSize(1000, 100);
+        contentPane.add(dropdownDept);
+
+        proceed = new JButton("Proceed");
+        proceed.setBorder(new EmptyBorder(5, 5, 5, 5));
+        proceed.setFont(new Font("Times New Roman", Font.PLAIN, 46));
+        proceed.setForeground(Color.BLACK);
+        proceed.setBackground(contentPane.getBackground());
+        proceed.setBorderPainted(true);
+        proceed.setSize(1000, 100);
+        // proceed.setMnemonic(KeyEvent.VK_A);
+        contentPane.add(proceed);
+        // proceed.addActionListener(new ActionListener(){
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         //TODO: Navigate outside
+        //         //Make a choice between switching panels and switching frames - whatever is better here.
+        //         // System.out.println("Pressed");
+        //         if(printmessage.isVisible()) {
+        //             printmessage.setVisible(false);
+        //         } else {
+        //             printmessage.setText("Button Pressed!");
+        //             printmessage.setVisible(true);
+        //         }
+        //     }        
+        // });
+        proceed.setFocusable(false);
+
+        printmessage = new JLabel();
+        printmessage.setForeground(Color.RED);
+        printmessage.setFont(new Font("Times New Roman", Font.PLAIN, 28));
+        // subheading.setBounds(80, 100, 500, 200);
+        printmessage.setVisible(true);
+        printmessage.setText("MySQL Connection Failed! Please try again later.");
+        contentPane.add(printmessage);
+
+    }
+
+    public HomeFrame(SQLConnection sqlConnection) throws SQLException {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 10, 2000, 1000);
+        setResizable(true);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new FlowLayout(FlowLayout.CENTER));
+        setContentPane(contentPane);
+
+        // tryoutlist = new String[];
+        Vector<String> depts = new Vector<>();
+        ResultSet resultSet = sqlConnection.executeQuery("select * from department");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+            depts.add(resultSet.getString("name"));
+        }
+        sqlConnection.closeConnection();
+
+        heading = new JLabel("Welcome to the Application!");
+        heading.setForeground(Color.BLACK);
+        heading.setFont(new Font("Times New Roman", Font.PLAIN, 96));
+        // heading.setBounds(80, 50, 500, 200);
+        heading.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.add(heading);
+
+        subheading = new JLabel("Here you can add course details of " + sem_val + " semester of year " + year_val);
+        subheading.setForeground(Color.BLACK);
+        subheading.setFont(new Font("Times New Roman", Font.PLAIN, 46));
+        // subheading.setBounds(80, 100, 500, 200);
+        contentPane.add(subheading);
+
+        selectDepartment = new JLabel("Please select a department");
+        selectDepartment.setForeground(Color.BLACK);
+        selectDepartment.setFont(new Font("Times New Roman", Font.PLAIN, 46));
+        // selectDepartment.setBounds(80, 100, 500, 200);
+        selectDepartment.setBorder(new EmptyBorder(100, 600, 150, 600));
+        contentPane.add(selectDepartment);
+
+        dropdownDept = new JComboBox<>(depts);
         // dropdownDept.setBounds(70, 200, 200, 100);
         dropdownDept.setBorder(new EmptyBorder(5, 5, 5, 5));
         dropdownDept.setFont(new Font("Times New Roman", Font.PLAIN, 40));
