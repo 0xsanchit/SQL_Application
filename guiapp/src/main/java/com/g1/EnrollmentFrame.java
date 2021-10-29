5865bb0567873208175eb5f60fdd93730c2a423e
VedantS01 <vedantsaboo2001@gmail.com>
2021-10-29 22:47:22 +0530

    v1.2.1.0

--- guiapp/src/main/java/com/g1/EnrollmentFrame.java
+++ guiapp/src/main/java/com/g1/EnrollmentFrame.java
@@ -1,6 +1,7 @@
 package com.g1;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,39 +11,40 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
+import java.awt.Color;
 
 public class EnrollmentFrame extends JFrame {
     private JLabel heading;
-    private JLabel l1, l2;
+    // private JLabel l1, l2;
     private JTextField rollnoField;
-    private JComboBox courseField;
+    private JComboBox<String> courseField;
     private JPanel contentPane;
     private String deptname;
     private String deptid;
+    private JButton process;
+
+    private Vector<String> cids = new Vector<>();
+    private Vector<String> cnames = new Vector<>();
     public EnrollmentFrame(SQLConnection sqlConnection, String depid) throws SQLException {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // setBounds(100, 10, 2000, 1000);
-        setExtendedState(MAXIMIZED_BOTH);
-        setResizable(false);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-        setContentPane(contentPane);
         this.deptid = depid;
 
-        heading = new JLabel();
-        heading.setFont(new Font("Times New Roman", Font.BOLD, 96));
-        heading.setBorder(new EmptyBorder(5, 200, 5, 200));
-        contentPane.add(heading);
         try {
             ResultSet resultSet = sqlConnection.executeQuery("select name from department d where d.deptId = "+depid);
             while(resultSet.next()) {
                 deptname = resultSet.getString("name");
                 // break;
             }
-            heading.setText("Department of " + deptname);
+
+            resultSet = sqlConnection.executeQuery("select * from course where deptNo = " + depid);
+            while(resultSet.next()) {
+                cids.add(resultSet.getString("courseId"));
+                cnames.add(resultSet.getString("cname"));
+            }
         } catch (Exception e) {
             e.printStackTrace();
             setVisible(false);
@@ -53,14 +55,69 @@ public class EnrollmentFrame extends JFrame {
             System.exit(0);
         }
 
-        l1 = new JLabel("Enter a roll number");
-        l1.setFont(new Font("Tahoma", Font.PLAIN, 40));
-        l1.setBorder(new EmptyBorder(5, 5, 50, 5));
-        contentPane.add(l1);
 
-        l2 = new JLabel("Select a course");
-        l2.setFont(new Font("Tahoma", Font.PLAIN, 40));
-        l2.setBorder(new EmptyBorder(5, 5, 50, 5));
-        contentPane.add(l2);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(450, 190, 1014, 597);
+        setResizable(false);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        // contentPane.scal
+        contentPane.setLayout(null);
+
+        JLabel lblNewLabel = new JLabel("Department of " + deptname);
+        lblNewLabel.setForeground(Color.BLACK);
+        lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 46));
+        lblNewLabel.setBounds(100, 13, 1000, 93);
+        contentPane.add(lblNewLabel);
+
+        rollnoField = new JTextField();
+        rollnoField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        rollnoField.setBounds(481, 170, 281, 68);
+        contentPane.add(rollnoField);
+        rollnoField.setColumns(10);
+
+        courseField= new JComboBox<>(cnames);
+        courseField.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        courseField.setBounds(481, 286, 281, 68);
+        contentPane.add(courseField);
+
+        JLabel lblUseruserName = new JLabel("Roll Number");
+        lblUseruserName.setBackground(Color.BLACK);
+        lblUseruserName.setForeground(Color.BLACK);
+        lblUseruserName.setFont(new Font("Tahoma", Font.PLAIN, 31));
+        lblUseruserName.setBounds(150, 170, 293, 52);
+        contentPane.add(lblUseruserName);
+
+        JLabel lblPassword = new JLabel("Select Course");
+        lblPassword.setForeground(Color.BLACK);
+        lblPassword.setBackground(Color.CYAN);
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 31));
+        lblPassword.setBounds(150, 286, 293, 52);
+        contentPane.add(lblPassword);
+
+        process = new JButton("Process");
+        process.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        process.setBounds(545, 392, 162, 73);
+        process.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //Do something
+                String rno = rollnoField.getText();
+                Integer index = courseField.getSelectedIndex();
+                String cid = cids.get(index);
+                try {
+                    String result = Query2.makeEnrollment(sqlConnection, rno, cid);
+                    JOptionPane.showMessageDialog(null, result);
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        contentPane.add(process);
+
+        // label = new JLabel("");
+        // label.setBounds(0, 0, 1008, 562);
+        // contentPane.add(label);
     }
 }
---
+++ guiapp/src/main/java/com/g1/Query2.java
@@ -0,0 +1,99 @@
+package com.g1;
+
+import java.sql.*;
+import java.util.*;
+
+public class Query2{
+
+    private static void recurse(HashSet<ArrayList<String>> p, String cid, HashSet<String> allpre)
+    {
+        String key,value;
+        for(ArrayList<String> itr : p)
+        {
+            key = itr.get(0);
+            value = itr.get(1);
+            if(value == cid && !allpre.contains(key))
+            {
+                recurse(p, key, allpre);
+                allpre.add(key);
+            }
+        }
+    }
+    private static boolean checkEligibility(HashSet<ArrayList<String>> p, String cid, HashSet<String> pset)
+    {
+        HashSet<String> allpreReq = new HashSet<String>();
+        recurse(p,cid,allpreReq);
+        for(String  itr : allpreReq)
+        {
+            if(!pset.contains(itr))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static String makeEnrollment(SQLConnection s,String rno,String cid) throws SQLException
+    {
+        String slistQuery = "SELECT rollNo FROM student";
+        ResultSet studentList = s.executeQuery(slistQuery);
+        HashSet<String> slist = new HashSet<String>();
+        while(studentList.next())
+        {
+            slist.add(studentList.getString("rollNo"));
+        }
+        if(!slist.contains(rno))
+        {
+            return "Unknown Student! Enrollment failed";
+        }
+
+        String clistQuery = "SELECT courseId FROM course";
+        ResultSet courseList = s.executeQuery(clistQuery);
+        HashSet<String> clist = new HashSet<String>();
+        while(courseList.next())
+        {
+            clist.add(courseList.getString("courseId"));
+        }
+        if(!clist.contains(cid))
+        {
+            return "Unknown Course! Enrollment failed";
+        }
+
+        String passlistQuery = "SELECT courseId FROM enrollment WHERE grade != 'U' AND grade != 'P' AND rollNo = \'" + rno + "\'";
+        // NO MORE RESTRICTIONS IN THE ABOVE QUERY AS 2006 EVEN SEM >= ALL ENROLLMENTS
+        ResultSet passList = s.executeQuery(passlistQuery);
+        HashSet<String> passSet = new HashSet<String>();
+        while(passList.next())
+        {
+            passSet.add(passList.getString("courseId"));
+        }
+
+        String preReqQuery = "SELECT preReqCourse AS p, courseId AS c FROM prerequisite";
+        ResultSet prereqRS = s.executeQuery(preReqQuery);
+        HashSet<ArrayList<String>> preRequisites = new HashSet<ArrayList<String>>();
+        HashSet<String>  probableCourses = new HashSet<String>();
+        String k,v;
+        while(prereqRS.next())
+        {
+            k = prereqRS.getString("p");
+            v = prereqRS.getString("c");
+            probableCourses.add(k);
+            probableCourses.add(v);
+            ArrayList<String> a = new ArrayList<>();
+            a.add(k);
+            a.add(v);
+            preRequisites.add(a);
+        }
+
+        if(probableCourses.contains(cid) && !checkEligibility(preRequisites,cid,passSet))
+        {
+           return "The student has not successfully completed all the required prerequisites! Enrollment failed";
+        }
+        else
+        {
+            //P for pending grade
+            String insertQuery = "INSERT INTO enrollment VALUES(\'"+rno+"\',\'"+cid+"\',\'even\',2001,\'P\')";
+            s.executeQuery(insertQuery);
+            return "The student has been successfully enrolled for the course";
+        }
+    }
+}
